01d2a49178b1aba5f0ef745acbf83eaa036c8452
Arthri <41360489+a@users.noreply.github.com>
2023-07-30 09:31:02 +0000

    Add project files

---
+++ src/Belp.SDK.Test.MSBuild.xUnit/MSBuildTest.cs
@@ -0,0 +1,101 @@
+﻿using FluentAssertions;
+using Microsoft.Build.Execution;
+using Xunit.Abstractions;
+
+namespace Belp.SDK.Test.MSBuild.XUnit;
+
+/// <summary>
+/// Provides the boilerplate for testing MSBuild projects.
+/// </summary>
+public class MSBuildTest
+{
+    /// <summary>
+    /// Inherits from <see cref="BuildParameters"/> and sets additional default values.
+    /// </summary>
+    protected class BuildParametersWithDefaults : BuildParameters
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="BuildParameters"/> with additional default values.
+        /// </summary>
+        public BuildParametersWithDefaults(MSBuildTest test) : this(test.Logger)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="BuildParameters"/> with additional default values.
+        /// </summary>
+        public BuildParametersWithDefaults(XUnitMSBuildLoggerAdapter logger)
+        {
+            EnableNodeReuse = true;
+            Loggers = Loggers == null
+                ? logger.AsSingletonEnumerable()
+                : Loggers.Append(logger)
+                ;
+        }
+    }
+
+    /// <summary>
+    /// Gets the test's logger.
+    /// </summary>
+    protected XUnitMSBuildLoggerAdapter Logger { get; }
+
+    /// <summary>
+    /// Initializes a new instance of <see cref="MSBuildTest"/>.
+    /// </summary>
+    /// <param name="output">The test output helper from xUnit.</param>
+    public MSBuildTest(ITestOutputHelper output)
+    {
+        Logger = new XUnitMSBuildLoggerAdapter(output);
+    }
+
+    /// <summary>
+    /// Requests the build of the project at the specified path with default parameters and default request data.
+    /// </summary>
+    /// <param name="projectPath">The path to the project to build.</param>
+    /// <returns>The result of the project build.</returns>
+    public BuildResult RequestBuild(string projectPath)
+    {
+        BuildResult result = BuildManager.DefaultBuildManager.Build(
+            new BuildParametersWithDefaults(this),
+            new BuildRequestData(
+                projectPath,
+                new Dictionary<string, string>(),
+                null,
+                new string[] { "Restore", "Build" }, null,
+                BuildRequestDataFlags.ProvideProjectStateAfterBuild
+            )
+        );
+
+        VerifyBuild(result);
+
+        return result;
+    }
+
+    /// <summary>
+    /// Requests the processing of the specified build request with default parameters.
+    /// </summary>
+    /// <param name="buildRequest">The request to process.</param>
+    /// <returns>The result of the build.</returns>
+    public BuildResult RequestBuild(BuildRequestData buildRequest)
+    {
+        BuildResult result = BuildManager.DefaultBuildManager.Build(
+            new BuildParametersWithDefaults(this),
+            buildRequest
+        );
+
+        VerifyBuild(result);
+
+        return result;
+    }
+
+    /// <summary>
+    /// Verifies the specified build result is successful.
+    /// </summary>
+    /// <param name="buildResult">The build result to verify.</param>
+    public static void VerifyBuild(BuildResult buildResult)
+    {
+        _ = buildResult.OverallResult.Should().Be(BuildResultCode.Success);
+
+        _ = buildResult.ProjectStateAfterBuild.Should().NotBeNull();
+    }
+}
---
+++ src/Belp.SDK.Test.MSBuild.xUnit/XUnitMSBuildLoggerAdapter.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Build.Framework;
+using Xunit.Abstractions;
+
+namespace Belp.SDK.Test.MSBuild.XUnit;
+
+/// <summary>
+/// Provides an adapter for <see cref="ITestOutputHelper"/> to <see cref="ILogger"/>.
+/// </summary>
+public class XUnitMSBuildLoggerAdapter : ITestOutputHelper, ILogger
+{
+    private readonly ITestOutputHelper _output;
+
+    string? ILogger.Parameters
+    {
+        get => null;
+
+        set
+        {
+        }
+    }
+
+    /// <inheritdoc />
+    public LoggerVerbosity Verbosity { get; set; } = LoggerVerbosity.Normal;
+
+    /// <summary>
+    /// Initializes a new instance of <see cref="XUnitMSBuildLoggerAdapter"/> for the specified <paramref name="output"/>.
+    /// </summary>
+    /// <param name="output">The <see cref="ITestOutputHelper"/> to adapt.</param>
+    public XUnitMSBuildLoggerAdapter(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    #region ILogger
+
+    void ILogger.Initialize(IEventSource eventSource)
+    {
+        Initialize(eventSource);
+    }
+
+    void ILogger.Shutdown()
+    {
+        Shutdown();
+    }
+
+    /// <inheritdoc />
+    public virtual void Initialize(IEventSource eventSource)
+    {
+        eventSource.BuildFinished += (sender, e) => WriteLine(e.Message);
+        eventSource.BuildStarted += (sender, e) => WriteLine(e.Message);
+        eventSource.ErrorRaised += (sender, e) => WriteLine($"ERR {e.Code}: {e.Message}");
+        eventSource.MessageRaised += (sender, e) =>
+        {
+            if (e.Importance == MessageImportance.High)
+            {
+                WriteLine(e.Message);
+            }
+        };
+        eventSource.ProjectFinished += (sender, e) => WriteLine(e.Message);
+        eventSource.ProjectStarted += (sender, e) => WriteLine(e.Message);
+        eventSource.WarningRaised += (sender, e) => WriteLine($"WRN {e.Code}: {e.Message}");
+    }
+
+    /// <inheritdoc />
+    public virtual void Shutdown()
+    {
+    }
+
+    #endregion
+
+    /// <inheritdoc />
+    public void WriteLine(string message)
+    {
+        _output.WriteLine(message);
+    }
+
+    /// <inheritdoc />
+    public void WriteLine(string format, params object[] args)
+    {
+        _output.WriteLine(format, args);
+    }
+}
