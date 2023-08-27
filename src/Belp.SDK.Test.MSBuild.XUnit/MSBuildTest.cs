726de498aa6ebc4f6ecd978b65f0664fc3314bdf
Arthri <41360489+a@users.noreply.github.com>
2023-08-27 13:24:48 +0000

    Move SingleEnumerable to MSBuildTest

--- src/Belp.SDK.Test.MSBuild.XUnit/MSBuildTest.cs
+++ src/Belp.SDK.Test.MSBuild.XUnit/MSBuildTest.cs
@@ -1,6 +1,7 @@
-using FluentAssertions;
+﻿using FluentAssertions;
 using Microsoft.Build.Execution;
 using Microsoft.Build.Framework;
+using System.Collections;
 using Xunit.Abstractions;
 
 namespace Belp.SDK.Test.MSBuild.XUnit;
@@ -153,5 +154,69 @@ file static class Extensions
             .ThenBy(static v => v.Code)
             .ThenBy(static v => v.Span)
             ;
+    }
+}
+
+file sealed class SingleEnumerable<T> : IEnumerable<T>
+{
+    private sealed class Enumerator : IEnumerator<T>
+    {
+        private readonly T _element;
+        private int _state;
+
+        public T Current => _state switch
+        {
+            0 => throw new InvalidOperationException("Enumeration has not started. Call MoveNext."),
+            1 => _element,
+            2 => throw new InvalidOperationException("Enumeration already finished."),
+            _ => throw new InvalidOperationException("Invalid state."),
+        };
+
+        object? IEnumerator.Current => Current;
+
+        public Enumerator(T element)
+        {
+            _element = element;
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+        }
+
+        /// <inheritdoc />
+        public bool MoveNext()
+        {
+            if (_state is >= 0 and <= 1)
+            {
+                _state++;
+                return _state == 1;
+            }
+
+            return false;
+        }
+
+        /// <inheritdoc />
+        public void Reset()
+        {
+            _state = 0;
+        }
+    }
+
+    private readonly T _element;
+
+    public SingleEnumerable(T element)
+    {
+        _element = element;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return new Enumerator(_element);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
     }
 }
